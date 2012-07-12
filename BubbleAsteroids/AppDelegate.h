209e02c6687b30ebfbf4a154e0a4e422d7afdad1
Eric Liang <Eric@dyn-car-138-65.dyn.columbia.edu>
2012-07-12 00:16:09 -0400

    Initial Commit

---
+++ BubbleAsteroids/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BubbleAsteroids
+//
+//  Created by Eric Liang on 7/12/12.
+//  Copyright (c) 2012 St.Stephen's. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
